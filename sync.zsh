8dc02bfd4d27d5a566730783a79c727c4e75f750
Jonathan Torres <jonathantorres41@gmail.com>
2018-04-08 01:26:12 -0400

    Run python script

--- sync.zsh
+++ sync.zsh
@@ -11,6 +11,7 @@ st_version='3'
 cp ~/Library/Application\ Support/Sublime\ Text\ $st_version/Packages/User/Preferences.sublime-settings ~/dev/dotfiles/st
 cp ~/Library/Application\ Support/Sublime\ Text\ $st_version/Packages/User/Default\ \(OSX\).sublime-keymap ~/dev/dotfiles/st
 echo "Copied settings from Sublime Text $st_version"
+python3 ~/dev/dotfiles/st_packages.py
 
 # Sync nginx
 cp /usr/local/etc/nginx/nginx.conf ~/dev/dotfiles/nginx/nginx.conf
