7158ed246eced237e3fed10c6c81a1f635a054e6
Jonathan Torres <jonathantorres41@gmail.com>
2020-02-05 21:04:41 -0500

    Rename sync file

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-if [[ -f "$HOME/bin/sync" ]]; then
-    rm -f "$HOME/bin/sync"
+if [[ -f "$HOME/bin/dotsync" ]]; then
+    rm -f "$HOME/bin/dotsync"
 fi
 
-ln -s ~/dev/dotfiles/sync.sh ~/bin/sync
+ln -s ~/dev/dotfiles/sync.sh ~/bin/dotsync
 echo 'Symlink created!';
 
